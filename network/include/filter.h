071804c992301c4a1e1fb615418fb9e83c762715
Jacob Zimmerman <jzimm@Jacobs-MBP.lan>
2022-05-07 01:04:06 -0400

    going to bed, filter feed needswork

---
+++ network/include/filter.h
@@ -0,0 +1,15 @@
+#ifndef FILTER_INCLUDED
+#define FILTER_INCLUDED
+
+#include "layer.h"
+#include "matrix.h"
+
+typedef struct Filter_T *Filter_T;
+
+extern Filter_T Filter_new(shape2_t filter_shape, shape2_t input_shape, int n_filters);
+extern void Filter_free(Filter_T *filter);
+
+extern bit2_t Filter_feed(Filter_T filter, bit2_t synapses);
+extern shape2_t Filter_shape(Filter_T filter);
+
+#endif
---
+++ network/src/filter.c
@@ -0,0 +1,62 @@
+#include "filter.h"
+#include "spikemath.h"
+
+struct Filter_T {
+    struct shape2_t f_shape; /* shape of each feature map */
+    struct shape2_t output_shape; /* overall shape */
+
+    int n_filters;
+
+    Layer_T *layers;
+    bit2_t inter_inhibition;
+
+    bit2_t spikes;
+};
+
+Filter_T Filter_new(shape2_t filter_shape, shape2_t input_shape, int n_filters)
+{
+    Filter_T filter = malloc(sizeof(struct Filter_T));
+
+    filter->f_shape = *filter_shape;
+    filter->output_shape.x = filter_shape->x * n_filters;
+    filter->output_shape.y = filter_shape->y;
+
+    filter->n_filters = n_filters;
+
+    filter->inter_inhibition = bit2_new(filter->f_shape.x, filter->f_shape.y);
+    filter->spikes = bit2_new(filter->output_shape.x, filter->output_shape.y);
+    filter->layers = malloc(sizeof(Layer_T) * n_filters);
+
+    for (int i = 0; i < n_filters; i++) {
+        filter->layers[i] = Layer_new(filter_shape, input_shape);
+        Layer_connect_inhibition(filter->layers[i], filter->inter_inhibition);
+    }
+
+    return filter;
+}
+
+void Filter_free(Filter_T *filter)
+{
+    for (int i = 0; i < (*filter)->n_filters; i++)
+        Layer_free(&(*filter)->layers[i]);
+
+    bit2_free(&(*filter)->inter_inhibition);
+    free(*filter);
+}
+
+bit2_t Filter_feed(Filter_T filter, bit2_t synapses)
+{
+    int n_filters = filter->n_filters;
+
+    bit2_t output = NULL;
+    for (int i = 0; i < n_filters; i++) {
+         output = Layer_feed(filter->layers[i], synapses);
+    }
+
+    return output;
+}
+
+shape2_t Filter_shape(Filter_T filter)
+{
+    return &filter->output_shape;
+}
