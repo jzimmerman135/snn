bf7241dc2e9d4f524f87e436e29b0fabe5d49d08
Jacob Zimmerman <jzimm@Jacobs-MBP.lan>
2022-05-10 10:59:57 -0400

    logging done

--- network/src/logger.c
+++ network/src/logger.c
@@ -3,6 +3,8 @@
 
 #define MAX_LINES 100000
 
+static void Log_print_head(Log_T log);
+
 Log_T Log_open(char *filename)
 {
     /* add path to filename */
@@ -18,6 +20,8 @@ Log_T Log_open(char *filename)
     log->time = 0;
     log->input_index = 0;
 
+    Log_print_head(log);
+
     return log;
 }
 
@@ -31,11 +35,18 @@ void Log_close(Log_T *log)
     *log = NULL;
 }
 
-/* example id: 401 means filter 3 layer 1
-    or     id: 2 means layer 2 outside of a filter
-           id: a0b means filter a - 1, layer b
-*/
 
+/*
+ *  log->time stores a continuous counter
+ *  to get number of neurons in a layer use Layer_size(layer)
+ *  to get number of other info use weights layer->voltages->data[i]
+ *  to get weights use layer->weights[i]->data[j] for the jth input neuron weight
+ *
+ *  example id: 401 means filter 3 layer 1
+ *       or id: 2 means layer 2 outside of a filter
+ *          id: a0b means filter a - 1, layer b
+ *
+ */
 void Log_layer(Log_T log, Layer_T layer, int id)
 {
     if (++log->line_count > MAX_LINES) {
@@ -45,7 +56,25 @@ void Log_layer(Log_T log, Layer_T layer, int id)
             return;
     }
 
-    fprintf(log->fp, "%i, %i, %i\n", Layer_size(layer), log->time, id);
+    int n_neurons = Layer_size(layer);
+    int n_input_neurons = layer->input_shape.x * layer->input_shape.y;
+    for (int i = 0; i < n_neurons; i++) {
+        int t           = log->time;
+        int input_idx   = log->input_index;
+        int idx         = i;
+        float v         = layer->voltages->data[i];
+        float stdp      = layer->voltages_stdp->data[i];
+        float *w        = layer->weights[i]->data;
+
+        fprintf(log->fp, "%i,%i,%i,%i,%.3f,%.3f,",
+                t, input_idx, idx, id, v, stdp);
+
+        for (int j = 0; j < n_input_neurons; j++) {
+            fprintf(log->fp, "%.3f,", w[j]);
+        }
+
+        fprintf(log->fp,"\b\n");
+    }
 }
 
 void Log_filter(Log_T log, Filter_T filter, int id)
@@ -55,7 +84,7 @@ void Log_filter(Log_T log, Filter_T filter, int id)
         Log_layer(log, filter->layers[i], id + ((i + 1) * 100));
 }
 
-void Log_print_head(Log_T log)
+static void Log_print_head(Log_T log)
 {
-    fprintf(log->fp, "a, b, c, d, e\n");
+    fprintf(log->fp, "t,input_idx,idx,id,v,stdp,[w]\n");
 }
